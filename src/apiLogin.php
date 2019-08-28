1b0e8ba2e52e0aaa25d2370af2f433e50a28cba6
Andrew Blocki <42648377+arblocki@users.noreply.github.com>
2019-08-28 12:07:17 -0400

    Update apiLogin.php

--- src/apiLogin.php
+++ src/apiLogin.php
@@ -4,10 +4,9 @@
 
 	// Fetch the saved access token from $_SESSION
 	$stmt = $pdo->query('SELECT * FROM users WHERE user_id = \''.$_SESSION['id'].'\'');
-	
 	$row = $stmt->fetch();
 
-	$session->refreshAccessToken(/*$_SESSION['refresh']*/$row['refresh']);
+	// Get access token and set it in API instance
+	$session->refreshAccessToken($row['refresh']);
 	$accessToken = $session->getAccessToken();
-
 	$api->setAccessToken($accessToken);
