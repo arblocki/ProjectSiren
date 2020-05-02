beb8558498f49a2f97932b3e7ba70cc7fe16ae8a
Andrew Blocki <arblocki@umich.edu>
2020-05-02 15:00:35 -0400

    Update apiLogin.php

--- src/apiLogin.php
+++ src/apiLogin.php
@@ -2,6 +2,12 @@
 
 	$api = new SpotifyWebAPI\SpotifyWebAPI();
 
+	if (!isset($_SESSION['id'])) {
+		// Redirect to login page 
+		header("Location: ../");
+		return;
+	}
+
 	// Fetch the saved access token from $_SESSION
 	$stmt = $pdo->query('SELECT * FROM users WHERE user_id = \''.$_SESSION['id'].'\'');
 	$row = $stmt->fetch();
